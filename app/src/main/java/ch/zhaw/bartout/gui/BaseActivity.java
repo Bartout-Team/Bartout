7893c1f12d05b77e22e77cd20d212c563f6e8b15
srueg <simon@rueggs.ch>
2015-03-24 23:38:07 +0100

    drawer

--- app/src/main/java/ch/zhaw/bartout/gui/BaseActivity.java
+++ app/src/main/java/ch/zhaw/bartout/gui/BaseActivity.java
@@ -1,34 +1,65 @@
 package ch.zhaw.bartout.gui;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import ch.zhaw.bartout.R;
 
 /**
  * Created by srueg on 24.03.15.
  */
-public abstract class BaseActivity extends Activity {
+public abstract class BaseActivity extends Activity implements ListView.OnItemClickListener{
 
-    private final int layoutId;
-    private final boolean homeAsUp;
+    private final int mLayoutId;
+    private final boolean mHomeAsUp;
+
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     public BaseActivity(int layoutId){
         this(layoutId, true);
     }
 
     public BaseActivity(int layoutId, boolean homeAsUp){
-        this.layoutId = layoutId;
-        this.homeAsUp = homeAsUp;
+        mLayoutId = layoutId;
+        mHomeAsUp = homeAsUp;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layoutId);
-        getActionBar().setDisplayHomeAsUpEnabled(homeAsUp);
+        setContentView(mLayoutId);
+        getActionBar().setDisplayHomeAsUpEnabled(mHomeAsUp);
+        getActionBar().setHomeButtonEnabled(true);
+
+        String[] drawerTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close){
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerToggle.syncState();
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, drawerTitles));
+        mDrawerList.setOnItemClickListener(this);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
     @Override
@@ -36,6 +67,11 @@ public abstract class BaseActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public void onItemClick(AdapterView parent, View view, int position, long id) {
+        mDrawerLayout.closeDrawer(mDrawerList);
     }
 
     @Override
@@ -51,6 +87,26 @@ public abstract class BaseActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
 }
