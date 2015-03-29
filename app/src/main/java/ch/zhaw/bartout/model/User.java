5f34e127d5d13cd8c5c79fd3f5f8c3ecedf32238
Beni Warmers <benjamin.warmers@gmail.com>
2015-03-29 16:49:07 +0200

    User model plain class UserStatus created

--- app/src/main/java/ch/zhaw/bartout/model/User.java
+++ app/src/main/java/ch/zhaw/bartout/model/User.java
@@ -4,4 +4,38 @@ package ch.zhaw.bartout.model;
  * Created by bwa on 29.03.2015.
  */
 public class User {
+    private String name;
+    private boolean man;
+    private int weight;
+    private UserStatus status;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public UserStatus getStatus() {
+        return status;
+    }
+
 }
