a92830bf1738cb7848835650799d5876c2b774cb
ruedenic <ruedenic@students.zhaw.ch>
2015-03-31 23:29:55 +0200

    ATMLocationEvent, EstablishmentLocationEvent, LocationEvent, RankingEvent and UserParticipationEvent created

---
+++ app/src/main/java/ch/zhaw/bartout/model/EstablishmentLocationEvent.java
@@ -0,0 +1,13 @@
+package ch.zhaw.bartout.model;
+
+/**
+ * Created by Nico on 31.03.2015.
+ */
+public class EstablishmentLocationEvent extends LocationEvent {
+    private String type;
+
+    public String getType() {   return type;   }
+
+    public void setType(String type) {  this.type = type;   }
+
+}
