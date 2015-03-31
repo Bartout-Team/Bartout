60fbad71f1c4db1b74697be40e2edc46b9eb4ed4
Simon Rüegg <simon@rueggs.ch>
2015-03-31 20:42:47 +0200

    link userFramgent with bartourActivity

--- app/src/main/java/ch/zhaw/bartout/controller/BartourActivity.java
+++ app/src/main/java/ch/zhaw/bartout/controller/BartourActivity.java
@@ -1,9 +1,13 @@
 package ch.zhaw.bartout.controller;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.net.Uri;
+import android.view.View;
+
 import ch.zhaw.bartout.R;
 
-public class BartourActivity extends BaseActivity {
-
+public class BartourActivity extends BaseActivity implements UserFragment.OnFragmentInteractionListener {
 
     public BartourActivity() {
         super(R.layout.activity_bartour);
@@ -12,5 +16,21 @@ public class BartourActivity extends BaseActivity {
     @Override
     protected int getNameRes() {
         return R.string.title_bartour;
+    }
+
+    public void addUserButtonOnClick(View view){
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        Fragment prev = getFragmentManager().findFragmentByTag("fragment_user");
+        if(prev != null){
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+        UserFragment newFragment = UserFragment.newInstance();
+        newFragment.show(ft, "fragment_user");
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
--- app/src/main/java/ch/zhaw/bartout/controller/UserFragment.java
+++ app/src/main/java/ch/zhaw/bartout/controller/UserFragment.java
@@ -1,6 +1,7 @@
 package ch.zhaw.bartout.controller;
 
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -19,33 +20,12 @@ import ch.zhaw.bartout.R;
  * Use the {@link UserFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class UserFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class UserFragment extends DialogFragment {
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UserFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UserFragment newInstance(String param1, String param2) {
+    public static UserFragment newInstance() {
         UserFragment fragment = new UserFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -56,24 +36,12 @@ public class UserFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
