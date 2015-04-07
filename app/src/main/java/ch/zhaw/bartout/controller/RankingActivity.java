97241c9c843fabceff652ccca69ec2bb4961df5b
Serge Schoop <schooser@students.zhaw.ch>
2015-04-07 14:03:51 +0200

    RankingActivity: Get Ranking by singleton instead of parameter

--- app/src/main/java/ch/zhaw/bartout/controller/RankingActivity.java
+++ app/src/main/java/ch/zhaw/bartout/controller/RankingActivity.java
@@ -5,11 +5,11 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import ch.zhaw.bartout.R;
+import ch.zhaw.bartout.model.Bartout;
 import ch.zhaw.bartout.model.Ranking;
 
 public class RankingActivity extends BaseActivity {
 
-    public static String EXTRA_RANKING = "ch.zhaw.bartout.controller.Ranking";
     private Ranking ranking;
     private ListView listView;
 
@@ -20,17 +20,14 @@ public class RankingActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        android.content.Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
-        ranking = (Ranking) bundle.getSerializable(EXTRA_RANKING);
 
+        ranking = Bartout.getInstance().getActiveBartour().getRanking();
         listView = (ListView) findViewById(R.id.list_view);
         RankingAdapter adapter = new RankingAdapter(
                 this,
                 ranking.getRanking()
         );
         listView.setAdapter(adapter);
-
     }
 
     @Override
