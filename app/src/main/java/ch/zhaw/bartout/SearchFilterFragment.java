6406b06162dc0435fcd7ad3a5a43950064446562
srueg <simon@rueggs.ch>
2015-04-21 16:00:34 +0200

    searchFilter fragment

---
+++ app/src/main/java/ch/zhaw/bartout/SearchFilterFragment.java
@@ -0,0 +1,83 @@
+package ch.zhaw.bartout;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SearchFilterFragment extends Fragment {
+
+    private static final String ARG_PARAM_FILTER_STRING = "paramFilterString";
+
+    private String filterString;
+
+    private OnFragmentInteractionListener mListener;
+
+    public static SearchFilterFragment newInstance(String filterString) {
+        SearchFilterFragment fragment = new SearchFilterFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM_FILTER_STRING, filterString);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public SearchFilterFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            filterString = getArguments().getString(ARG_PARAM_FILTER_STRING);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search_filter, container, false);
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
