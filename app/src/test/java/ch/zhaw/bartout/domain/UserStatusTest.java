fd03c77f4cadec9e0d0a431ac64a4fd45a2d7664
Serge Schoop <schooser@students.zhaw.ch>
2015-05-01 18:25:58 +0200

    Tests are now finally running! UnitTest and Instrumentation Test

---
+++ app/src/test/java/ch/zhaw/bartout/domain/UserStatusTest.java
@@ -0,0 +1,50 @@
+package ch.zhaw.bartout.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created by serge on 01.05.2015.
+ */
+public class UserStatusTest {
+
+    UserStatus userStatus;
+
+    @Before
+    public void setUp() throws Exception {
+        userStatus = mock(UserStatus.class);
+    }
+
+    @Test
+    public void testFitToDrive() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAlcoholLevel() throws Exception {
+
+    }
+
+    @Test
+    public void testFitToDriveDuration() throws Exception {
+
+    }
+
+    @Test
+    public void testAddConsumption() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveConsumption() throws Exception {
+
+    }
+
+    @Test
+    public void testGetConsumptions() throws Exception {
+
+    }
+}
