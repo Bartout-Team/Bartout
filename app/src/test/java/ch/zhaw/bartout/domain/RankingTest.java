26cb4cd78127e1cb33069462f565901271f18de0
Beni Warmers <benjamin.warmers@gmail.com>
2015-05-02 00:05:32 +0200

    RankingTest created

---
+++ app/src/test/java/ch/zhaw/bartout/domain/RankingTest.java
@@ -0,0 +1,14 @@
+package ch.zhaw.bartout.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RankingTest {
+
+    @Test
+    public void testUpdateRanking() throws Exception {
+        assertTrue(true);
+
+    }
+}
