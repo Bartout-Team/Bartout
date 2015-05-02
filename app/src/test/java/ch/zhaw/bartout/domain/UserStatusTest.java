50f8d5b14fe95883182eb3983dae21f3ddb0b5c3
Serge Schoop <schooser@students.zhaw.ch>
2015-05-02 18:20:07 +0200

    add PowerMock to gradle (for static mock functionality) force junit 4.12 version for version conflicts

--- app/src/test/java/ch/zhaw/bartout/domain/UserStatusTest.java
+++ app/src/test/java/ch/zhaw/bartout/domain/UserStatusTest.java
@@ -2,25 +2,59 @@ package ch.zhaw.bartout.domain;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 /**
  * Created by serge on 01.05.2015.
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Chronicle.class)
 public class UserStatusTest {
 
-    UserStatus userStatus;
+    User userManMock;
+    User userWomanMock;
+    Chronicle activeChronicleMock;
 
     @Before
     public void setUp() throws Exception {
-        userStatus = mock(UserStatus.class);
+        userManMock = mock(User.class);
+        when(userManMock.getWeight()).thenReturn(65);
+        when(userManMock.isMan()).thenReturn(true);
+        userWomanMock = mock(User.class);
+        when(userWomanMock.getWeight()).thenReturn(65);
+        when(userWomanMock.isMan()).thenReturn(false);
+
+        activeChronicleMock = mock(Chronicle.class);
+        when(activeChronicleMock.getChronicleEvents()).thenReturn(new ArrayList<ChronicleEvent>());
+
+        mockStatic(Chronicle.class);
+        when(Chronicle.getActiveChronicle()).thenReturn(activeChronicleMock);
     }
 
     @Test
-    public void testFitToDrive() throws Exception {
+    public void testFitToDriveFalse() throws Exception {
+        UserStatus userStatus = new UserStatus(userManMock);
+        userStatus.addConsumption(new Consumption("", 5, 5));
+        assertEquals(true, userStatus.fitToDrive());
+/*        userStatus = new UserStatus(userWomanMock);
+        userStatus.addConsumption(new Consumption("", 5, 6));
+        assertFalse(userStatus.fitToDrive());*/
+    }
 
+    @Test
+    public void testFitToDriveTrue() throws Exception {
+/*        UserStatus userStatus = new UserStatus(userManMock);
+        userStatus.addConsumption(new Consumption("", 5, 6));
+        assertTrue(userStatus.fitToDrive());*/
     }
 
     @Test
@@ -43,8 +77,4 @@ public class UserStatusTest {
 
     }
 
-    @Test
-    public void testGetConsumptions() throws Exception {
-
-    }
 }
