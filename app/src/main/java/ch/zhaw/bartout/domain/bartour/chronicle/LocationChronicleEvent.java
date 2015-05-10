efde97a143851c99db0db6090ed97e67e425884d
ruedenic <ruedenic@students.zhaw.ch>
2015-05-10 16:01:24 +0200

    Javadoc for LocationChronicleEvent

--- app/src/main/java/ch/zhaw/bartout/domain/bartour/chronicle/LocationChronicleEvent.java
+++ app/src/main/java/ch/zhaw/bartout/domain/bartour/chronicle/LocationChronicleEvent.java
@@ -6,7 +6,7 @@ import ch.zhaw.bartout.domain.bartour.chronicle.ChronicleEvent;
 import se.walkercrou.places.Place;
 
 /**
- * Created by Nico on 31.03.2015.
+ * The class holds events that are related to a location.
  */
 public abstract class LocationChronicleEvent extends ChronicleEvent {
 
