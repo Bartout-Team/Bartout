2fe0c7b7ba7275df3739f01f16bf1f16452cd1a9
Beni Warmers <benjamin.warmers@gmail.com>
2015-05-11 17:57:50 +0200

    signing added and test data uncommented

--- app/src/main/java/ch/zhaw/bartout/gui/HomeActivity.java
+++ app/src/main/java/ch/zhaw/bartout/gui/HomeActivity.java
@@ -38,7 +38,7 @@ public class HomeActivity extends BaseActivity implements AdapterView.OnItemClic
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bartout = Bartout.getInstance();
-        bartout.initializeSampleData();
+        //bartout.initializeSampleData();
     }
 
     public void newBartourButtonOnClick(View view){
